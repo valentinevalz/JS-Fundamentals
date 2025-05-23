cafeb5e5e3ada6d9e1cbb444866600bd62c530fc
valentinevalz <odedeyivaz@gmail.com>
2025-05-23 10:05:47 +0100

    Fix: print value of argument instead of literal string

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,6 +1,8 @@
 const Firstargs = process.argv[2];
 
-if( Firstargs === undefined ) {
-console.log("No Argument");
+if (Firstargs === undefined) {
+  console.log("No argument");
+} else {
+  console.log(Firstargs); // print the value of the variable
 }
-else { console.log("Firstargs"); }
+
