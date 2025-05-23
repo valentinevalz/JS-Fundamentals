63c3a4db2a552e2fcedb42d886c7b7c02f184e96
valentinevalz <odedeyivaz@gmail.com>
2025-05-23 14:52:36 +0100

    Fix: correct argument count and print message format

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,10 +1,10 @@
-const args = process.argv.length
+const args = process.argv.length - 2;
 
-if (args <= 0) { console.log("No Arguments");
+if (args === 0) {
+  console.log("No argument");
+} else if (args === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
 }
-
-else if (args === 1) { console.log("Arguments Found");
-} 
-else { console.log("Arguments Found"); }
-
 
