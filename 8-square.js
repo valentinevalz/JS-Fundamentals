b93e90cec7379826ff5a6f6bbce4db55d8330ea6
valentinevalz <odedeyivaz@gmail.com>
2025-05-23 10:45:25 +0100

    Fix: correct case and error message for printing square

--- 8-square.js
+++ 8-square.js
@@ -1,4 +1,10 @@
-const size = parseInt(process.argv[2]);  
-if(isNaN(size)) {console.log("missing size");
+const size = parseInt(process.argv[2]);
 
-}else { for (let i = 0; i < size; i++) { console.log("x".repeat(size));} }
+if (isNaN(size)) {
+  console.log("Missing size");
+} else {
+  for (let i = 0; i < size; i++) {
+    console.log("X".repeat(size));
+  }
+}
+
