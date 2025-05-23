45f4a53f20c712d817763dfafc8ed788241f429a
valentinevalz <odedeyivaz@gmail.com>
2025-05-23 10:34:45 +0100

    Fix: correct spelling, capitalization, and loop behavior in task 7

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,8 +1,10 @@
 const times = parseInt(process.argv[2]);
 
-if (isNaN(times)) { console.log("missing number of occurenccies");
-}else {
-
-for (let i = 0; i < times; i++){
-console.log("c is fun"); }
+if (isNaN(times)) {
+  console.log("Missing number of occurrences");
+} else {
+  for (let i = 0; i < times; i++) {
+    console.log("C is fun");
+  }
 }
+
